332ac4946016f2a5dcdcc1763b5a85b572831b6c
peperon1028 <106125007+peperon031028@users.noreply.github.com>
2022-11-27 15:19:33 +0900

    MapBox

--- main.js
+++ main.js
@@ -1,118 +1,32 @@
 console.log("main.js!!");
 
-let snd1 = null //サウンド１
-let snd2 = null //サウンド２
-let snd3 = null //サウンド３
-let snd4 = null //サウンド４
-let snd5 = null //サウンド５
-let snd6 = null //サウンド６
-let snd7 = null //サウンド７
-let snd8 = null //サウンド８
-let snd9 = null //サウンド９
+const ATTRIBUTION = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+const ACCESS_TOKEN = "mapbox";
+
 
 
 // Ready
 $(document).ready(()=>{
 	console.log("Ready!!");
 
-	//サウンド１オブジェクトを作る
-	snd1 = new Howl({
-		src:["./sounds/taiko1.mp3"],
-		loop: false,
-		volume: 1.0
-	});
-	console.log(snd1)
+	// 緯度経度データ
+	const nichibi = [35.383938980172594, 136.60834026856318]; //にちび
+	const oogaki = [35.36675123324879, 136.6178258973979];　//大垣　
+	const Hawaii = [19.571253360686654, -155.96236690114216];//ハワイ
+	const London = [51.50865180157388, -0.12634837477805755];//ロンドン
 
-	snd2 = new Howl({
-		src:["./sounds/taiko2.mp3"],
-		loop: false,
-		volume: 1.0
-	});
-	console.log(snd2)
+	// マップを作る
+	let map = L.map("mapid").setView(nichibi, 16);
 
-	snd3 = new Howl({
-		src:["./sounds/taiko3.mp3"],
-		loop: false,
-		volume: 1.0
-	});
-	console.log(snd3)
+	// マップの設定
+	L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+		attribution: ATTRIBUTION,// 著作権表記
+		accessToken: ACCESS_TOKEN,// アクセストークン
+		id: "mapbox/streets-v11",// マップの種類
+	}).addTo(map);
 
-	snd4 = new Howl({
-		src:["./sounds/don.mp3"],
-		loop: false,
-		volume: 1.0
-	});
-	console.log(snd4)
-
-	snd5 = new Howl({
-		src:["./sounds/doramu.mp3"],
-		loop: false,
-		volume: 1.0
-	});
-	console.log(snd5)
-
-	snd6 = new Howl({
-		src:["./sounds/roru.mp3"],
-		loop: false,
-		volume: 1.0
-	});
-	console.log(snd6)
-
-	snd7 = new Howl({
-		src:["./sounds/piano.mp3"],
-		loop: false,
-		volume: 1.0
-	});
-	console.log(snd7)
-
-	snd8 = new Howl({
-		src:["./sounds/suzu.mp3"],
-		loop: false,
-		volume: 1.0
-	});
-	console.log(snd8)
-
-	snd9 = new Howl({
-		src:["./sounds/tyan.mp3"],
-		loop: false,
-		volume: 1.0
-	});
-	console.log(snd9)
-
-	$("#snd1").click(() => {
-		snd1.play()
-	})
-
-	$("#snd2").click(() => {
-		snd2.play()
-	})
-	
-	$("#snd3").click(() => {
-		snd3.play()
-	})
-
-	$("#snd4").click(() => {
-		snd4.play()
-	})
-
-	$("#snd5").click(() => {
-		snd5.play()
-	})
-
-	$("#snd6").click(() => {
-		snd6.play()
-	})
-
-	$("#snd7").click(() => {
-		snd7.play()
-	})
-
-	$("#snd8").click(() => {
-		snd8.play()
-	})
-
-	$("#snd9").click(() => {
-		snd9.play()
-	})
-
+	L.marker(oogaki).addTo(map).bindPopup("大垣駅").openPopup();
+	L.marker(nichibi).addTo(map).bindPopup("日本総合ビジネス専門学校").openPopup();
+	L.marker(Hawaii).addTo(map).bindPopup("ハワイ").openPopup();
+	L.marker(London).addTo(map).bindPopup("ロンドン").openPopup();
 });
