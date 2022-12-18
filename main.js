439e9d9b72bd89ac544dbe3402640dabcc62ddb5
peperon1028 <106125007+peperon031028@users.noreply.github.com>
2022-12-18 12:22:34 +0900

    割り勘　円指定　１２月１８日

--- main.js
+++ main.js
@@ -1,52 +1,40 @@
 console.log("main.js!!");
 
-$(document).ready(()=>{
-	console.log("Ready!!");
+$('#unit').change(function() {
+	var r = $('option:selected').val();
 	
-	// Geolocation APIに対応している
-/*if( navigator.geolocation )
-{
-	// 現在位置を取得できる場合の処理
-	alert( "あなたの端末では、現在位置を取得することができます。" ) ;
-}
+	console.log(r);
+})
+    
+$("#btn").click(() => {
+	doAnimation("#result",  "animate__backInUp");
+	var r = $('option:selected').val();
 
-// Geolocation APIに対応していない
-else
-{
-	// 現在位置を取得できない場合の処理
-	alert( "あなたの端末では、現在位置を取得できません。" ) ;
-}*/
+	if(r == 1){
+		
+		$("#result").text($("#price").val() / $("#num").val() + "円" )
+		console.log("1円単位")
 
-navigator.geolocation.getCurrentPosition((position)=> {
-    //緯度
-    let lat = position.coords.latitude;
-    //経度
-    let lng = position.coords.longitude;
-	//時間(ミリ秒)
-	let Time = position.timestamp;
-});
+		//一円を指定すると１円単位で計算する
+	}
+	else if(r == 10){
+		$("#result").text($("#price").val() / 10 / $("#num").val() + "円")
+		console.log("10円単位")
 
+		//10円を指定すると１0円単位で計算する
+	}
+	else if(r == 100){
+		$("#result").text($("#price").val() / 100 / $("#num").val() + "円")
+		console.log("100円単位")
 
-//緯度・経度・時間に使うドキュメントを取得しておく
-let lat = document.getElementById('lat');
-let lng = document.getElementById('lng');
-let Time = document.getElementById('Time');
-let map = document.getElementById('map');
+		//100円を指定すると１00円単位で計算する
+	}
+	else if(r == 1000){
+		$("#result").text($("#price").val() / 1000 / $("#num").val() + "円")
+		console.log("1000円単位")
 
-navigator.geolocation.getCurrentPosition((position)=>{   
-
-    let coords = position.coords;
-
-
-	//htmlに出力する
-    lat.textContent = coords.latitude;
-    lng.textContent = coords.longitude;
-	Time.textContent = position.timestamp;
-
-    map.setAttribute('href', `https://www.google.co.jp/maps/@${coords.latitude},${coords.longitude},15z`);
-});
-
-// 現在位置を取得する
-navigator.geolocation.getCurrentPosition( successFunc , errorFunc , optionObj ) ;
+		//1000円を指定すると1000円単位で計算する
+	}
 
 });
+
