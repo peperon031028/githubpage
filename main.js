594781113d2419f96875572d39142ccabaf54b79
peperon1028 <106125007+peperon031028@users.noreply.github.com>
2023-01-09 09:46:22 +0900

    webspeech　動いたり動かなかったりする

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("main.js!!");
+
+$(document).ready(()=>{
+	console.log("Ready!!");
+	
+	//Windowsに対応させる
+	SpeechRecognition = webkitSpeechRecognition || SpeechRecognition;
+if ('SpeechRecognition' in window) {
+  console.log("ユーザのブラウザは音声合成に対応しています。")
+} else {
+  console.log("ユーザのブラウザは音声合成に対応していません。")
+}
+
+//返される結果の中身を確かめる
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+  console.log(event);
+}
+recognition.start();
+});
