e484633df84ff9107f2473e9049e85e2814f5c97
peperon1028 <106125007+peperon031028@users.noreply.github.com>
2023-04-19 11:40:52 +0900

    うるう年計算　アルゴリズム

--- main.js
+++ main.js
@@ -1,71 +1,41 @@
 console.log("main.js!!");
 
-//==========
-// p5.jsを使ってみよう
-// 		公式サイト: https://p5js.org/
-//		Get Started: https://p5js.org/get-started/
-//		References: https://p5js.org/reference/
+/**
+ * 基本情報技術者アルゴリズム対策
+ * 練習: 03
+ * デバッガでのトレースで動きを確かめること
+*/
+//全体のコードをGistで報告する
+let isleap = (y)=>{
+    console.log("isleap:", y);
+    let leap = false
 
-
-function draw(){
-
-
-    for(let i = 0; i < 1; i++) {
-        let a = random(1,3);
-        let b = random(1,3);
-        let c = random(1,3);
-        let d = random(1,3);
-        randomBall(a,b,c,d)
-        console.log(a,b,c,d)
+    if(y % 400 == 0){
+        leap = true
     }
-    noLoop()
-}
-
-	console.log("Ready!!");
-
-  /**
- * p5.js連続して実行される関数
- */
-
-
-function randomBall(a,b,c,d){
-    var ball = document.getElementById("ball")
-    ,bh = ball.offsetHeight
-    ,bw = ball.offsetWidth
-    ,left = ( window.innerWidth / a ) - ( bw / b )
-    ,top = -ball.offsetHeight
-    ,ground = ( window.innerHeight / c ) - ( bh / d );
-    ball.style.left = left + "px";
-    ball.style.top = top + "px";
-  
-    var y = top
-,b = ground
-,v = -20
-,R = 0.9;
-function gravity() {
-y+= ++v;
-if( y >= b ) {
-y = b;
-v*= -R;
-if ( !~~v ) {
-clearInterval(g)
-}
-}
-document.getElementById("ball").style.top = y + "px";
-}
-var g = setInterval(gravity, 10);
-
-y+= ++v;
-
-document.getElementById("ball").style.top = y + "px";
-
-if( y >= b ) {
-    y = b;
-    v*= -R;
-    if ( !~~v ) {
-    clearInterval(g)
+    else if(y % 4　== 0){
+        if(y % 100 != 0){
+            leap = true
+        }
+        else{
+            leap = false
+        }
     }
+    
+    else{
+        leap = false
     }
 
+    if(leap == false){
+        return "うるう年ではない"
+    }
+    else{
+        return "うるう年である"
+    }
 }
 
+console.log("Result", isleap(2024))
+console.log("Result", isleap(2023))
+console.log("Result", isleap(2022))
+console.log("Result", isleap(2021))
+console.log("Result", isleap(2020))
