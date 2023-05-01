76e7acd98238acad1dc616b9291abd3d8d3c37b0
peperon1028 <106125007+peperon031028@users.noreply.github.com>
2023-05-02 06:31:06 +0900

    アルゴリズム　一応

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 console.log("main.js!!");
+
+/**
+ * 基本情報技術者アルゴリズム対策
+ * 練習: 08
+ * デバッガでのトレースで動きを確かめること
+*/
+let d = 123114
+
+let summation  = (n)=>{
+    let y = 0
+    let z = 1
+    while(d > 0){
+        x = moth.floor(d % 10 / 10)
+        if(x = 0){
+            y = y + 5 * z
+        }
+        else{
+            y = y + x * z
+        }
+        console.log(y)
+    }   
+}
