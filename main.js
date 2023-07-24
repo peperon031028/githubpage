762a548d7e69076f0a201a32aa32415859a314a3
peperon1028 <106125007+peperon031028@users.noreply.github.com>
2023-07-24 15:40:24 +0900

    ガソリン代　計算　課題

--- main.js
+++ main.js
@@ -10,35 +10,24 @@ $("#btn").click(() => {
 	
 		  //左側inputの入力値を取得（numA）
 		  var numA = parseInt($("#num").val());
-		  numA = numA / 100
+		  console.log(numA)
+		  Number(numA)
+
 		  var numB = parseInt($("#num2").val());
-		  var numC = ((numB / (numA * numA)));
+		  console.log(numB)
+		  Number(numB)
+
+		  var numC = parseInt($("#num3").val());
+		  console.log(numC)
 		  Number(numC)
 
-		  if (numC < 18.5) {
-			message = "低体重（やせ型）";
-			message2 = "もう少し体重を増やしましょう。";
-		  } else if (numC >= 18.5 && numC < 25) {
-			message = "普通体重";
-			message2 = "標準的な体重の範囲です。";
-		  } else if (numC >= 25 && numC < 30) {
-			message = "肥満(level1)";
-			message2 = "肥満体重です。BMIが25を超えると、糖尿病や高血圧などの生活習慣病のリスクが2倍以上になるとされています。※ ただし必ずしも医学的に減量すべきとは限りません。";
-		  } else if (numC >= 30 && numC < 35) {
-			message = "肥満(level2)";
-			message2 = "肥満体重です。BMIは25を超えると糖尿病や高血圧などの生活習慣病のリスクが2倍以上になり、30を超えると積極的な減量治療が必要とされています。";
-		  } else if (numC >= 35 && numC < 40) {
-			message = "肥満(level3)";
-			message2 = "高度肥満です。今すぐ医師の診断を受けましょう。";
-		  } else if (numC >= 40) {
-			message = "肥満(level4)";
-			message2 = "高度肥満です。今すぐ医師の診断を受けましょう。";
-		  }
-	
-		  $("#list1").text("BMI値は" + numC + "です。");
-		  $("#list2").text("診断結果：" + message);
-		  $("#list3").text(message2);
-		  console.log(numC);  
+		  numD = Math.round(numA * numB / numC);
+		  Number(numD)
+		
+		  console.log(numD)
+
+		  $("#list1").text("ガソリン代は" + numD + "円");
+		  $("#list2").text("ガソリン価格をリッター" + numA + "円として計算すると、燃費リッター" + numB + "kmの車で" + numC + "走った時のガソリン代は" + numD + "円です。");
 		  
 	
 	})
