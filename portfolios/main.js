f2ab088ee752f86720d2db5f3c935474e9defb8d
peperon1028 <106125007+peperon031028@users.noreply.github.com>
2023-10-31 12:31:47 +0900

    ポートフォリオ

--- portfolios/main.js
+++ portfolios/main.js
@@ -1,8 +1,14 @@
 console.log("main.js!!");
+// Ready
+$(document).ready(()=>{
+	console.log("Ready!!");
+	AOS.init();// AOSを初期化します
+});
+//logoの表示
+$(window).on("load" ,function(){
+    //ローディング画面を1.5秒(1500ms)待機してからフェイドアウト
+    $("#splash").delay(1500).fadeOut("slow");
+    //ロゴを1.2秒(1200ms)待機してからフェイドアウト
+    $("#splash_logo").delay(1200).fadeOut("slow")
+})
 
-window.onload = ()=>{
-	console.log("onload!!");
-
-	// Modal
-	
-}
