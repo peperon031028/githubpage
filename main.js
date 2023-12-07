1f1c3789c5bc653f7cce075d2d6b9566af0bb5b0
peperon1028 <106125007+peperon031028@users.noreply.github.com>
2023-12-07 12:16:38 +0900

    動画の載せ方

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
 console.log("main.js!!");
-// Ready
-$(document).ready(()=>{
-	console.log("Ready!!");
-	AOS.init();// AOSを初期化します
-});
-//logoの表示
-$(window).on("load" ,function(){
-    //ローディング画面を1.5秒(1500ms)待機してからフェイドアウト
-    $("#splash").delay(1500).fadeOut("slow");
-    //ロゴを1.2秒(1200ms)待機してからフェイドアウト
-    $("#splash_logo").delay(1200).fadeOut("slow")
-})
-
